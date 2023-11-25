48bf609ce5542236614cd77cf5acc3ca1b755867
junroot.kim <junroot.kim@navercorp.com>
2023-11-25 22:57:25 +0900

    feat: spa로 수정

--- front/src/routes/+layout.js
+++ front/src/routes/+layout.js
@@ -1,3 +1,4 @@
 // This can be false if you're using a fallback (i.e. SPA mode)
-export const prerender = true;
+export const prerender = "auto";
 export const trailingSlash = "always";
+export const ssr = false;
