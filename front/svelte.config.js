ea89207bb614aa2d54042920815a12aa46d82d77
junroot.kim <junroot.kim@navercorp.com>
2023-11-25 20:44:07 +0900

    feat: ssg -> spa로 변경

--- front/svelte.config.js
+++ front/svelte.config.js
@@ -13,7 +13,7 @@ const config = {
       // these options are set automatically — see below
       pages: "build",
       assets: "build",
-      fallback: undefined,
+      fallback: "index.html",
       precompress: false,
       strict: true,
     }),
