edcf481ff7c4a046a7715964b76bbd02f03435a7
Junroot <junroot0909@gmail.com>
2024-01-06 16:57:35 +0900

    fix: me 애니메이션 수정

--- front/src/components/lobby/Me.vue
+++ front/src/components/lobby/Me.vue
@@ -1,21 +1,21 @@
 <template>
-  <button
-    style="width: 80px; height: 80px"
-    @mouseenter="onMouseEnter"
-    @mouseleave="onMouseLeave"
-  >
+  <button style="width: 80px; height: 80px">
     <v-sheet
-      :height="80"
-      class="d-flex flex-row align-center justify-center profile rounded-pill"
-      :elevation="16"
+      :height="defaultProfileWidth"
+      :width="profileWidth"
+      @mouseenter="onMouseEnter"
+      @mouseleave="onMouseLeave"
+      ref="profile"
+      class="d-flex flex-row align-center profile rounded-pill"
+      :class="{ 'elevation-16': isHover }"
     >
-      <v-sheet class="flex-grow-0 flex-shrink-0" :width="80">
+      <v-sheet class="flex-grow-0 flex-shrink-0" :width="defaultProfileWidth">
         <v-sheet class="ma-auto" :width="64">
           <UserIcon></UserIcon>
         </v-sheet>
       </v-sheet>
       <v-sheet
-        v-if="isHover"
+        ref="nickname"
         class="flex-grow-0 flex-shrink-0 pr-8 text-h4 font-weight-medium nickname"
         >{{ nickname }}</v-sheet
       >
@@ -25,6 +25,7 @@
 
 <script lang="ts">
 import UserIcon from "@/icons/UserIcon.vue";
+import { VSheet } from "vuetify/lib/components/index.mjs";
 
 export default {
   components: {
@@ -39,7 +40,23 @@ export default {
   data() {
     return {
       isHover: false,
+      defaultProfileWidth: 80,
+      profileWidth: 80,
     };
+  },
+  watch: {
+    isHover(newIsHover) {
+      if (!newIsHover) {
+        this.profileWidth = this.defaultProfileWidth;
+        return;
+      }
+      let nicknameRef = this.$refs.nickname as VSheet;
+      if (!nicknameRef) {
+        return;
+      }
+      this.profileWidth =
+        nicknameRef.$el.clientWidth + this.defaultProfileWidth;
+    },
   },
   methods: {
     onMouseEnter() {
@@ -59,11 +76,16 @@ button {
 
 .profile {
   position: absolute;
+  overflow: hidden;
   top: 0;
-  transition: all 0.3s;
+  transition: all 0.3s ease;
 }
 
 .profile:hover {
   background-color: var(--tertiary-100);
+}
+
+.nickname {
+  display: inline-block;
 }
 </style>
