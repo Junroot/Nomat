537a5a7792c66f0d181d065b1fb177057da61300
Junroot <junroot0909@gmail.com>
2024-02-04 15:36:23 +0900

    test3

--- back/src/main/kotlin/ilpak/nomat/user/dto/UserIdAndNickname.kt
+++ back/src/main/kotlin/ilpak/nomat/user/dto/UserIdAndNickname.kt
@@ -1,6 +1,10 @@
 package ilpak.nomat.user.dto
 
 data class UserIdAndNickname(
-										val userId: Long,
+	val userId: Long,
 	val nickname: String
-)
+) {
+	fun abc() {
+		userId.dec().dec().dec().dec().dec().dec().dec().dec().dec().dec().dec().dec().dec().dec().dec().dec().dec().dec().dec().dec().dec().dec().dec().dec().dec().dec().dec().dec().dec().dec().dec().dec().dec()
+	}
+}
