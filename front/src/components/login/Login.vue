2793bc98330ec31a369cf52bd2d1ec0ff7eaf3e7
Hs <41941629+hsrst@users.noreply.github.com>
2024-06-23 23:14:00 +0900

    Feature/hs/login page (#60)
    * 로그인 페이지 추가.
    
    * 로그인 페이지 추가.

---
+++ front/src/components/login/Login.vue
@@ -0,0 +1,49 @@
+<template>
+    <v-sheet class="w-screen h-screen d-flex flex-column justify-center align-center">
+      <v-sheet class="text-h4 font-weight-bold mb-3">{{title}}</v-sheet>
+      <v-sheet class="mb-6">{{info_message}}</v-sheet>
+      <v-sheet class="d-flex flex-row ga-6">
+        <LoginServiceButton><DiscordIcon></DiscordIcon></LoginServiceButton>
+        <LoginServiceButton><NaverIcon></NaverIcon></LoginServiceButton>
+        <LoginServiceButton><GoogleIcon></GoogleIcon></LoginServiceButton>
+      </v-sheet>
+    </v-sheet>
+</template>
+
+<script lang="ts">
+import DiscordIcon from "@/icons/DiscordIcon.vue";
+import NaverIcon from "@/icons/NaverIcon.vue";
+import GoogleIcon from "@/icons/GoogleIcon.vue";
+import LoginServiceButton from "@/components/login/LoginServiceButton.vue";
+
+export default {
+  components: {
+    DiscordIcon,
+    NaverIcon,
+    GoogleIcon,
+    LoginServiceButton,
+  },
+    props: {
+      title: {
+        type: String,
+        required: true,
+      },
+      info_message: {
+        type: String,
+        required: true,
+      },
+    },
+  methods: {
+    onClickBack(){
+      window.location.href = "/";
+    }
+  }
+  };
+</script>
+
+<style>
+.discord-button {
+  background-color: var(--primary-400);
+  color: var(--font-light);
+}
+</style>
---
+++ front/src/components/login/LoginServiceButton.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-sheet class="rounded-lg">
+        <v-sheet class="d-flex flex-row mb-7">
+            <v-sheet :width="60" class="flex-grow-0 flex-shrink-0 mr-2">
+                <slot></slot>
+            </v-sheet>
+        </v-sheet>
+    </v-sheet>
+</template>
+
+<style>
+.discord-button {
+  background-color: var(--primary-400);
+  color: var(--font-light);
+}
+</style>
