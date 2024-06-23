e20035ff04cb54f1c576d8c0236d4590639568e0
Hs <hyosangkim@dgist.ac.kr>
2024-06-23 18:54:43 +0900

    로그인 페이지 추가.

--- front/src/components/login/Login.vue
+++ front/src/components/login/Login.vue
@@ -1,20 +1,26 @@
 <template>
     <v-sheet class="w-screen h-screen d-flex flex-column justify-center align-center">
       <v-sheet class="text-h4 font-weight-bold mb-3">{{title}}</v-sheet>
-      <v-sheet class="mb-8">{{info_message}}</v-sheet>
-      <LoginServiceButton label="Discord로 로그인"><DiscordIcon></DiscordIcon></LoginServiceButton>
-      <LoginServiceButton label="네이버로 로그인"><DiscordIcon></DiscordIcon></LoginServiceButton>
-      <LoginServiceButton label="Google로 로그인"><DiscordIcon></DiscordIcon></LoginServiceButton>
+      <v-sheet class="mb-6">{{info_message}}</v-sheet>
+      <v-sheet class="d-flex flex-row ga-6">
+        <LoginServiceButton><DiscordIcon></DiscordIcon></LoginServiceButton>
+        <LoginServiceButton><NaverIcon></NaverIcon></LoginServiceButton>
+        <LoginServiceButton><GoogleIcon></GoogleIcon></LoginServiceButton>
+      </v-sheet>
     </v-sheet>
 </template>
 
 <script lang="ts">
 import DiscordIcon from "@/icons/DiscordIcon.vue";
+import NaverIcon from "@/icons/NaverIcon.vue";
+import GoogleIcon from "@/icons/GoogleIcon.vue";
 import LoginServiceButton from "@/components/login/LoginServiceButton.vue";
 
 export default {
   components: {
     DiscordIcon,
+    NaverIcon,
+    GoogleIcon,
     LoginServiceButton,
   },
     props: {
--- front/src/components/login/LoginServiceButton.vue
+++ front/src/components/login/LoginServiceButton.vue
@@ -1,24 +1,12 @@
 <template>
     <v-sheet class="rounded-lg">
         <v-sheet class="d-flex flex-row mb-7">
-            <v-sheet :width="28" class="flex-grow-0 flex-shrink-0 mr-2">
+            <v-sheet :width="60" class="flex-grow-0 flex-shrink-0 mr-2">
                 <slot></slot>
             </v-sheet>
-            <v-sheet>{{ label }}</v-sheet>
         </v-sheet>
     </v-sheet>
 </template>
-
-<script lang="ts">
-export default {
-    props: {
-      label: {
-        type: String,
-        required: true,
-      },
-    },
-  };
-</script>
 
 <style>
 .discord-button {
