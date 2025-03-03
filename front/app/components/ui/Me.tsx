4befd6dcc337347d8f5f5cb86e3db615388e9e51
Junroot <junroot0909@gmail.com>
2025-03-03 21:15:03 +0900

    fix: 내 프로필 CSS 수정

--- front/app/components/ui/Me.tsx
+++ front/app/components/ui/Me.tsx
@@ -28,7 +28,7 @@ export default function Me(properties: MeProperties) {
 
     return <div className="me size-[96px] p-[8px]">
         <div 
-            className={`h-[80px] m-[8px] flex flex-row cursor-pointer profile transition-all transform rounded-full ${isHover ? "drop-shadow-lg" : ""}`}
+            className={`h-[80px] flex flex-row cursor-pointer profile transition-all transform rounded-full ${isHover ? "drop-shadow-lg" : ""}`}
             onMouseEnter={ () => setHover(true) }
             onMouseLeave={ () => setHover(false) }
             style={{ width: `${width}px` }}
