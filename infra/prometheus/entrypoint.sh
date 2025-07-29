1274d2a2706593fe655b38b637ea72e2dccd2874
Junroot <junroot0909@gmail.com>
2025-07-29 10:47:50 +0900

    fix: prom dockerfile

--- infra/prometheus/entrypoint.sh
+++ infra/prometheus/entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-sed -i "s|\${DATA_NODE_IP}|${DATA_NODE_IP}|g" /etc/prometheus/prometheus.yml
+sed -i "s|\${DATA_NODE_IP}|${DATA_NODE_IP}|g" /etc/prometheus/prometheus.yml > /etc/prometheus/prometheus.yml.tmp
+
+mv /etc/prometheus/prometheus.yml.tmp /etc/prometheus/prometheus.yml
 
 echo "--- Generated prometheus.yml ---"
 cat /etc/prometheus/prometheus.yml
 echo "------------------------------"
 
-exec "$@"
+exec prometheus --config.file=/etc/prometheus/prometheus.yml --storage.tsdb.path=/prometheus
