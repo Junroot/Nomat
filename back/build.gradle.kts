c712e91c0faa2bc7a634ee6a1481a213de5ffa8d
Junroot <junroot0909@gmail.com>
2025-09-16 00:00:06 +0900

    feat: spring-kafka 의존성 추가

--- back/build.gradle.kts
+++ back/build.gradle.kts
@@ -40,6 +40,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 	implementation("org.springframework.data:spring-data-elasticsearch")
 	implementation("org.springframework.retry:spring-retry")
+	implementation("org.springframework.kafka:spring-kafka")
 	implementation("io.jsonwebtoken:jjwt-api:0.12.6")
 	implementation("net.logstash.logback:logstash-logback-encoder:7.4")
 	implementation("ch.qos.logback.access:logback-access-common:2.0.6")
